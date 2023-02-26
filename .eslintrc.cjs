03d7ab48a41b4fb06b7ae17dbedf55b34ee2219e
sweetim <hosweetim@gmail.com>
2023-02-26 15:31:06 +0900

    added ignore pattern in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,14 @@
 module.exports = {
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint"
+    ],
+    ignorePatterns: [
+        "lib/**"
+    ],
     root: true,
 }
