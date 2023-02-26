b6e5e88ef7cdafad5ab62c4f478aac943e92adf4
sweetim <hosweetim@gmail.com>
2023-02-26 21:00:09 +0900

    refactor transform buffer

--- src/streams/top-info-transform.test.ts
+++ src/streams/top-info-transform.test.ts
@@ -1,7 +1,77 @@
-import { describe, it, expect } from "vitest"
+import {
+    describe,
+    it,
+    expect,
+} from "vitest"
 
-describe("topInfoTransform", () => {
-    it("should buffer the input", () => {
-        expect(1).toBe(1)
+import { Readable } from "stream"
+
+import { bufferTillDelimiterTransform } from "./top-info-transform"
+
+describe("bufferTillDelimiterTransform", () => {
+    it("should buffer the input", async () => {
+        const getAllDataFromStream = (input: string[]) => {
+            return new Promise(resolve => {
+                let output = []
+
+                const rawDataStream = Readable.from("")
+                input.forEach(msg => rawDataStream.push(msg))
+
+                rawDataStream.pipe(
+                    bufferTillDelimiterTransform(/(?=^top)/gm)
+                )
+                .on("data", (data) => {
+                    console.log(data)
+                    output.push(data)
+                })
+                .on("end", () => {
+                    console.log("end")
+                    resolve(output)
+                })
+            })
+        }
+
+        const input = [
+`top
+abc
+def
+ghi
+`,
+`top
+abc`,
+`def`,
+`ghi`,
+`
+jkl`,
+`top
+abc
+def
+ghi
+
+top
+123
+456`
+        ]
+
+        const expected = [
+`top
+abc
+def
+ghi
+`,
+`top
+abcdefghi
+jkl`,
+`top
+abc
+def
+ghi
+
+`
+        ]
+
+        const actual = await getAllDataFromStream(input)
+        console.log(actual)
+        expect(actual).toStrictEqual(expected)
     })
 })
--- src/streams/top-info-transform.ts
+++ src/streams/top-info-transform.ts
@@ -8,22 +8,51 @@ import { parseTopInfo } from ".."
  * @returns transform stream
  */
 export function topInfoTransform(toString = false): Transform {
+    return bufferTillDelimiterTransform(
+        /(?=^top)/gm,
+        (buffer) => {
+            const topInfo = parseTopInfo(buffer)
+
+            return toString
+                ? JSON.stringify(topInfo)
+                : topInfo
+        }
+    )
+}
+
+/**
+ * it will always wait for next delimiter before emitting any data
+ * @param delimiter {RegEx} regular expression for delimiter
+ * @param mappingFn  a mapping function
+ * @returns transform stream
+ */
+export function bufferTillDelimiterTransform(
+    delimiter: RegExp,
+    mappingFn: (buffer: string) => any = (buffer: string) => buffer): Transform
+{
     let buffer = ""
-    const matcher = /(?=^top)/gm
 
     return new Transform({
         objectMode: true,
         transform(chunk: Buffer, encoding, callback) {
-            const isMatch = chunk.toString().match(matcher)
+            chunk.toString()
+                .split(delimiter)
+                .forEach(msg => {
+                    const isDelimiter = delimiter.test(msg)
 
-            if (isMatch) {
-                buffer = chunk.toString()
-            } else {
-                const topInfo =  parseTopInfo(buffer + chunk.toString())
-                const output = toString ? JSON.stringify(topInfo) : topInfo
-                this.push(output)
-                buffer = ""
-            }
+                    if (buffer && isDelimiter) {
+                        this.push(mappingFn(buffer))
+                        buffer = ""
+                    }
+
+                    if (isDelimiter) {
+                        buffer = msg
+
+                        return
+                    }
+
+                    buffer += msg
+                })
 
             callback()
         },
