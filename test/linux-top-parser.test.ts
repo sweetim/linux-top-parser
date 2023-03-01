0fd3a7ee37dec2152ca115b39c3b989bc2155141
sweetim <hosweetim@gmail.com>
2023-03-02 00:45:21 +0900

    added topInfoTransform test

--- test/linux-top-parser.test.ts
+++ test/linux-top-parser.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "vitest"
-import { readFileSync } from "fs"
+import { readFileSync, createReadStream } from "fs"
 import { resolve } from "path"
 
-import { parseTopInfo, TopInfo } from "../src"
+import { parseTopInfo, TopInfo, topInfoTransform } from "../src"
 
 function getDataFilePath(fileName: string): string {
     const DATA_FOLDER_NAME = "data"
@@ -36,6 +36,56 @@ describe("parseTopInfo", () => {
         })
 
         expect(parseTopInfo(input)).toStrictEqual(expected)
+    })
+
+    it("should throw error on empty input", () => {
+        const input = ""
+
+        expect(() => parseTopInfo(input)).toThrowError()
+    })
+})
+
+describe("topInfoTransform", () => {
+    it.each([
+        {
+            inputFile: "single.txt",
+            expectedFile: "single-expected.json"
+        },
+        {
+            inputFile: "multi.txt",
+            expectedFile: "multi-expected.json"
+        },
+        {
+            inputFile: "multi-all.txt",
+            expectedFile: "multi-all-expected.json"
+        }
+    ])("should be able to parse top output using stream from file ($inputFile) to be same as ($expectedFile)", async ({ inputFile, expectedFile }) => {
+        const getTopInfoFromStream = (inputFile): Promise<TopInfo[]> => {
+            return new Promise((resolve) => {
+                const output: TopInfo[] = []
+
+                createReadStream(getDataFilePath(inputFile))
+                    .pipe(topInfoTransform())
+                    .on("data", (data) => {
+                        output.push(data)
+                    })
+                    .on("end", () => {
+                        resolve(output)
+                    })
+            })
+        }
+
+        const expected: TopInfo[] = JSON.parse(
+            readFileSync(getDataFilePath(expectedFile)).toString()
+        ).map(item => {
+            item.summaryDisplay.upTimeAndLoadAverage.time = new Date(
+                item.summaryDisplay.upTimeAndLoadAverage.time)
+
+            return item
+        })
+
+        const actual = await getTopInfoFromStream(inputFile)
+        expect(actual.flat()).toStrictEqual(expected)
     })
 
     it("should throw error on empty input", () => {
