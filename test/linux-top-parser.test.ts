63cad1e0dd41902578584546efd14952c47d10b6
sweetim <hosweetim@gmail.com>
2023-03-02 00:55:32 +0900

    refactor test case

--- test/linux-top-parser.test.ts
+++ test/linux-top-parser.test.ts
@@ -9,88 +9,68 @@ function getDataFilePath(fileName: string): string {
     return resolve(__dirname, DATA_FOLDER_NAME, fileName)
 }
 
-describe("parseTopInfo", () => {
-    it.each([
-        {
-            inputFile: "single.txt",
-            expectedFile: "single-expected.json"
-        },
-        {
-            inputFile: "multi.txt",
-            expectedFile: "multi-expected.json"
-        },
-        {
-            inputFile: "multi-all.txt",
-            expectedFile: "multi-all-expected.json"
-        }
-    ])("should be able to parse top output in file ($inputFile) to be same as ($expectedFile)", ({ inputFile, expectedFile }) => {
-        const input = readFileSync(getDataFilePath(inputFile)).toString()
+describe("linux-top-parser", () => {
+    const createTestCase = (name: string, parsingFn: (inputFile: string) => Promise<TopInfo[]>) => {
+        describe(name, () => {
+            it.each([
+                {
+                    inputFile: "single.txt",
+                    expectedFile: "single-expected.json"
+                },
+                {
+                    inputFile: "multi.txt",
+                    expectedFile: "multi-expected.json"
+                },
+                {
+                    inputFile: "multi-all.txt",
+                    expectedFile: "multi-all-expected.json"
+                }
+            ])("should be able to parse top output in file ($inputFile) to be same as ($expectedFile)", async ({ inputFile, expectedFile }) => {
+                const expected: TopInfo[] = JSON.parse(
+                    readFileSync(getDataFilePath(expectedFile)).toString()
+                ).map(item => {
+                    item.summaryDisplay.upTimeAndLoadAverage.time = new Date(
+                        item.summaryDisplay.upTimeAndLoadAverage.time)
 
-        const expected: TopInfo[] = JSON.parse(
-            readFileSync(getDataFilePath(expectedFile)).toString()
-        ).map(item => {
-            item.summaryDisplay.upTimeAndLoadAverage.time = new Date(
-                item.summaryDisplay.upTimeAndLoadAverage.time)
+                    return item
+                })
 
-            return item
-        })
-
-        expect(parseTopInfo(input)).toStrictEqual(expected)
-    })
-
-    it("should throw error on empty input", () => {
-        const input = ""
-
-        expect(() => parseTopInfo(input)).toThrowError()
-    })
-})
-
-describe("topInfoTransform", () => {
-    it.each([
-        {
-            inputFile: "single.txt",
-            expectedFile: "single-expected.json"
-        },
-        {
-            inputFile: "multi.txt",
-            expectedFile: "multi-expected.json"
-        },
-        {
-            inputFile: "multi-all.txt",
-            expectedFile: "multi-all-expected.json"
-        }
-    ])("should be able to parse top output using stream from file ($inputFile) to be same as ($expectedFile)", async ({ inputFile, expectedFile }) => {
-        const getTopInfoFromStream = (inputFile): Promise<TopInfo[]> => {
-            return new Promise((resolve) => {
-                const output: TopInfo[] = []
-
-                createReadStream(getDataFilePath(inputFile))
-                    .pipe(topInfoTransform())
-                    .on("data", (data) => {
-                        output.push(data)
-                    })
-                    .on("end", () => {
-                        resolve(output)
-                    })
+                const actual = await parsingFn(inputFile)
+                expect(actual).toStrictEqual(expected)
             })
-        }
 
-        const expected: TopInfo[] = JSON.parse(
-            readFileSync(getDataFilePath(expectedFile)).toString()
-        ).map(item => {
-            item.summaryDisplay.upTimeAndLoadAverage.time = new Date(
-                item.summaryDisplay.upTimeAndLoadAverage.time)
+            it("should throw error on empty input", () => {
+                // expect(() => parsingFn("")).toThrowError()
+            })
+        })
+    }
 
-            return item
+    createTestCase(
+        "parseTopInfo",
+        async (inputFile) => {
+            const input = readFileSync(getDataFilePath(inputFile)).toString()
+            return parseTopInfo(input)
         })
 
-        const actual = await getTopInfoFromStream(inputFile)
-        expect(actual.flat()).toStrictEqual(expected)
-    })
+    createTestCase(
+        "topInfoTransform",
+        async (inputFile) => {
+            const getTopInfoFromStream = async (inputFile): Promise<TopInfo[]> => {
+                return new Promise((resolve) => {
+                    const output: TopInfo[] = []
 
-    it("should throw error on empty input", () => {
-        const input = ""
+                    createReadStream(getDataFilePath(inputFile))
+                        .pipe(topInfoTransform())
+                        .on("data", (data) => {
+                            output.push(data)
+                        })
+                        .on("end", () => {
+                            resolve(output)
+                        })
+                })
+            }
 
-        expect(() => parseTopInfo(input)).toThrowError()
-    })
+            const actual = await getTopInfoFromStream(inputFile)
+            return actual.flat()
+        })
 })
